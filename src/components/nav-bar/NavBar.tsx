14a3787968a560ff68c313890b5b297cb5a0b81a
Mbah Ndumbi home computer <kidndumbi@gmail.com>
2022-02-16 20:47:13 -0500

    nav stuff

--- src/components/nav-bar/NavBar.tsx
+++ src/components/nav-bar/NavBar.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren, useContext, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { CurrentPageContext } from "../../contexts/currentPage.context";
 
 /* eslint-disable jsx-a11y/anchor-is-valid */
@@ -27,7 +27,10 @@ const NavBar = ({ onSearch }: PropsWithChildren<Props>) => {
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container">
-          <a className="navbar-brand">Goals Manager</a>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <a className="navbar-brand">Goals Manager</a>
+          </Link>
+
           <button
             className="navbar-toggler"
             type="button"
@@ -42,30 +45,42 @@ const NavBar = ({ onSearch }: PropsWithChildren<Props>) => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page">
-                  Home
-                </a>
+                <Link to="/" style={{ textDecoration: "none" }}>
+                  <a className="nav-link active" aria-current="page">
+                    Home
+                  </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link">Reports</a>
+                <Link to="/reports" style={{ textDecoration: "none" }}>
+                  <a className="nav-link">Reports</a>
+                </Link>
               </li>
               <li className="nav-item">
-                <button className="btn btn-primary" type="button" onClick={() => { navigate('/addGoal')  }}>
+                <button
+                  className="btn btn-primary"
+                  type="button"
+                  onClick={() => {
+                    navigate("/addGoal");
+                  }}
+                >
                   New Goal
                 </button>
               </li>
             </ul>
-            { currentPage === 'MainPage' && <form className="d-flex">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-                onChange={(event) => {
-                  setSearchValue(event.target.value);
-                }}
-              />
-            </form>}
+            {currentPage === "MainPage" && (
+              <form className="d-flex">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                  onChange={(event) => {
+                    setSearchValue(event.target.value);
+                  }}
+                />
+              </form>
+            )}
           </div>
         </div>
       </nav>
