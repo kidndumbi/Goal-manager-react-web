dd2fc8c47c087c08badc2c3c6bfef07030bd06f9
Mbah Ndumbi home computer <kidndumbi@gmail.com>
2022-02-23 22:21:59 -0500

    objective

---
+++ src/components/objective/Objective.tsx
@@ -0,0 +1,74 @@
+import { Button, Card } from "react-bootstrap";
+import Moment from "react-moment";
+import { useSelector } from "react-redux";
+
+
+type ObjectiveProps = {
+    data: any;
+    onEdit: (objective: any) => void;
+    onMarkedForDelete: (objective: any) => void;
+}
+
+const Objective: React.FC<ObjectiveProps> = ({ data, onEdit }) => {
+  const statusOptions = useSelector(
+    (state: any) => state.statusOptions.options
+  );
+
+  return (
+    <>
+      <Card>
+        <Card.Header>{data.name}</Card.Header>
+        <Card.Body>
+          <Card.Title>
+
+          <div className="d-flex justify-content-between">
+        <span>Special title treatment</span>
+        <Button variant="danger">Delete</Button>
+      </div>
+          </Card.Title>
+          <Card.Text>
+            <div>
+              <div className="pb-2">
+                <strong>Due Date: </strong>
+                <span>
+                  <Moment format="dddd Do MMMM YYYY h:mm A">
+                    {data?.dueDate}
+                  </Moment>
+                </span>
+              </div>
+              <label htmlFor="edit-goal-status">
+                {" "}
+                <strong>Status</strong>{" "}
+              </label>
+              <select
+                id="edit-goal-status"
+                className="form-control"
+                value={data?.status}
+                autoFocus={true}
+              >
+                {statusOptions.map(
+                  (status: { name: string; value: string }) => {
+                    return (
+                      <option value={status.value}> {status.name} </option>
+                    );
+                  }
+                )}
+              </select>
+              <div className="pt-2">
+                <strong>Created On: </strong>
+                <span>
+                  <Moment format="dddd Do MMMM YYYY h:mm A">
+                    {data?.createDate}
+                  </Moment>
+                </span>
+              </div>
+            </div>
+          </Card.Text>
+          <Button variant="primary" onClick={onEdit.bind(null, data)}>Edit</Button>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
+
+export { Objective };
