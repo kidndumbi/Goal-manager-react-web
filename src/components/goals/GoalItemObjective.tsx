7267c8d2c491a512348068a08c451e22706ce575
Mbah Ndumbi home computer <kidndumbi@gmail.com>
2022-04-06 20:17:22 -0400

    objective due color

--- src/components/goals/GoalItemObjective.tsx
+++ src/components/goals/GoalItemObjective.tsx
@@ -1,4 +1,8 @@
 import { ObjectiveModel } from "../../models/ObjectiveModel.interface";
+import {
+  checkIfDateGreater,
+  checkIfDatesAreEqual,
+} from "../../utils/dateTimeHelpers";
 import { NotesIcon } from "../notes-icon/NotesIcon";
 export function GoalItemObjective({
   objective,
@@ -7,22 +11,33 @@ export function GoalItemObjective({
   objective: ObjectiveModel;
   className: string;
 }) {
+  const getGoalItemColor = (dueDate: number) => {
+    if (checkIfDateGreater(new Date(), new Date(dueDate!))) {
+      return "red";
+    }
+
+    if (checkIfDatesAreEqual(new Date(dueDate!), new Date())) {
+      return "yellow";
+    }
+
+    return "";
+  };
+
   return (
-    <li style={{listStyle: 'none'}}
+    <li
+      style={{ listStyle: "none" }}
       className={`${className} ${
         objective.status === "COMPLETE" && "text-decoration-line-through"
       } `}
     >
-
+      <span style={{ color: getGoalItemColor(objective?.dueDate!) }}>
+        {objective.name}
         <span>
-          {objective.name}
-          <span>
-            {objective?.notes && (
-              <NotesIcon className="ms-2" text={objective?.notes} />
-            )}
-          </span>
+          {objective?.notes && (
+            <NotesIcon className="ms-2" text={objective?.notes} />
+          )}
         </span>
-
+      </span>
     </li>
   );
 }
